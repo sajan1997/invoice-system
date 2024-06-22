d0870157ae8e2f961c6fd60cf80c4b6ae492bdd6
sajan1997 <sajanbappal@gmail.com>
2024-06-22 12:16:10 +0530

    initial commit

---
+++ src/main/java/com/system/invoice_system_service/InvoiceSystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.system.invoice_system_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceSystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceSystemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/invoice_system_service/InvoiceSystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.invoice_system_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceSystemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
